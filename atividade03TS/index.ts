b3c1b38eb96911d99301d6d6210423bb147099ad
joaooliveiradev <joaooliveira1775@gmail.com>
2022-02-24 12:08:49 -0300

    atividade de typescript

---
+++ atividade03TS/index.ts
@@ -0,0 +1,5 @@
+import Customer from './src/models/Customer';
+
+let customer: Customer = new Customer("Leandro", "1321312", 10)
+console.log('Cliente')
+console.log(customer)
---
+++ atividade03TS/src/models/Customer.ts
@@ -0,0 +1,16 @@
+export default class Customer {
+    private name: string
+    private phone: string
+    private age: number
+
+    constructor(name: string, phone: string, age: number){
+        this.name = name
+        this.phone = phone;
+        this.age = age;
+    }
+    setName = (name: string) => {
+        if(name.indexOf(" ") >= 0){
+            this.name = name;
+        }
+    } 
+}
